54ff7c467a905e462d76c55f565d32d404f67692
Ricardo Mubarak <psycho696@gmail.com>
2021-03-29 21:44:15 -0300

    Adicionar arquivos de projeto.

---
+++ contaCorrente/Conta.cs
@@ -0,0 +1,33 @@
+﻿namespace contaCorrente
+{
+    class Conta
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        public bool Saca(double valor)
+        {
+            if (this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public void Transferir(double valor , double origem, double destino)
+        {
+            this.saldo -= valor;
+            origem = saldo;
+            destino *= valor;
+
+            
+        }
+    }
+}
---
+++ contaCorrente/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace contaCorrente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saque, deposito, transferir;
+
+            Conta ricardo = new Conta();
+            ricardo.numero = 1;
+            ricardo.titular = "Ricardo";
+            ricardo.saldo = 150.00;
+
+            Conta raquel = new Conta();
+            raquel.numero = 1;
+            raquel.titular = "raquel";
+            raquel.saldo = 200.00;
+
+
+            Console.WriteLine($"Informaçoes da conta: \n\nNumero da conta: {ricardo.numero} \nTitular: {ricardo.titular} \nSaldo: {ricardo.saldo}");
+
+            //deposito
+            Console.WriteLine("\n\nQuanto deseja depositar?");
+            deposito = double.Parse(Console.ReadLine());
+            ricardo.Depositar(deposito);
+            Console.WriteLine($"\n\nDeposito de {deposito} realizado com sucesso");
+            Console.WriteLine($"\nNovo saldo: {ricardo.saldo}");
+
+            //saque
+            Console.WriteLine("\n\nQuanto deseja sacar?");
+            saque = double.Parse(Console.ReadLine());
+            bool deuCerto = ricardo.Saca(saque);
+            if (deuCerto)
+            {
+                Console.WriteLine("Saque realizado com sucesso");
+                Console.WriteLine($"Novo saldo: {ricardo.saldo}");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+            
+            //transferir 
+            Console.WriteLine("Quanto deseja transferir para raquel?");
+            transferir = double.Parse(Console.ReadLine());
+            ricardo.Transferir(transferir, ricardo.saldo, raquel.saldo);
+            Console.WriteLine($"\nnovo saldo ricardo: {ricardo.saldo} \nnovo saldo raquel {raquel.saldo}");
+            Console.ReadLine();
+        }
+
+    }
+}
